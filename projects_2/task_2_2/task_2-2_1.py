d96d46deacba069cf4644ee7149ae91375f57e21
Arkadiy Eroshkov <137702231+WinSherwood@users.noreply.github.com>
2026-02-26 23:54:27 +0300

    Add files via upload

---
+++ projects_2/task_2_2/task_2-2_1.py
@@ -0,0 +1,4 @@
+course_name = "Информатика"
+task_number = "2.2"
+finished_tasks_number = 9
+print(f"Название курса: {course_name}, номер задания: {task_number}, количество выполненных заданий: {finished_tasks_number}")
---
+++ projects_2/task_2_2/task_2-2_3.py
@@ -0,0 +1,5 @@
+staff_name = "Микроскоп"
+staff_number = 32
+staff_status = "Исправен"
+staff_amount = 90
+print(f"Название прибора\tИнвентарный номер\tСостояние\tКоличество\n{staff_name}\t\t\t{staff_number}\t\t{staff_status}\t{staff_amount}")
