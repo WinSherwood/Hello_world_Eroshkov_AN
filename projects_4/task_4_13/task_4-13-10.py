bf6b333545b8230d9b9882498f6ffd1428c1e97d
Arkadiy Eroshkov <137702231+WinSherwood@users.noreply.github.com>
2026-04-10 10:31:52 +0300

    Delete projects_4/task_4_13 directory

--- projects_4/task_4_13/task_4-13-10.py
+++
@@ -1,9 +0,0 @@
-arr = [float(i) for i in input("Введите числа через пробел: ").split()]
-sum = 0
-
-for i in range(len(arr)):
-    if i % 2 != 0:          
-        sum += arr[i]     
-
-
-print("Сумма элементов с нечётными индексами:", sum)
